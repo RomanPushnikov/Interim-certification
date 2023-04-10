df85f4e6dc79819e947afe66c0fd03f52e29653f
Roman Pushnikov <r.pushnikov@mail.ru>
2023-04-10 14:47:46 +0300

    Add and generate array

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿// // Вводим массив из строк,
 Console.Write("Enter string after shift: ");
 
+string? str = Console.ReadLine();                       // вводим строку
+var array = str.Split(" ");                             // строку делим по пробелу
+string[] arrayStr = new string [array.Length];          // объявление и инициализация массива
+
+for (int i = 0; i < array.Length; i++)                  // Сгенерировали массив
+{
+    arrayStr[i] = Convert.ToString(array[i]);
+}
+
